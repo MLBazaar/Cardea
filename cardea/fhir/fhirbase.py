d0b74aa49d8324284985a85deb7a358008a13bac
Najatabd <NajatAbd@users.noreply.github.com>
2018-08-20 17:31:48 -0400

    added a nan/empty checking function

--- cardea/fhir/fhirbase.py
+++ cardea/fhir/fhirbase.py
@@ -18,6 +18,13 @@ class fhirbase(object):
 
     def assert_type(self):
         pass
+    
+    def check_nan(self,dict_values):
+        number_of_nan =[]
+        for attr, value in dict_values.items():
+            number_of_nan.append({attr: sum(value in ['null', nan, 'nan','NAN','Nan','NaN', 'undefined', 'unknown'] for value in value)})
+        return number_of_nan
+            
 
     def get_relationships(self):
         return []
