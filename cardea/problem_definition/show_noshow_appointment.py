ce376d9a645f25a11e420d2fe52d8f3d3076deb4
Shahad Althobaiti <shahad@mit.edu>
2018-09-28 13:59:06 +0300

    update generate_cutoff_times method

--- cardea/problem_definition/show_noshow_appointment.py
+++ cardea/problem_definition/show_noshow_appointment.py
@@ -2,13 +2,14 @@
 from cardea.problem_definition import ProblemDefinition
 
 
-class PredictingMissedAppointmet (ProblemDefinition):
+class MissedAppointmentProblemDefinition (ProblemDefinition):
     """Defines the problem of missed appointments,
         whether the patient showed to the appointment or not.
 
     Attributes:
         target_label: The target label of the prediction problem.
-        target_entity: The entity name in which it contains the target label.
+        target_entity: The entity name which contains the target label.
+        prediction_type: The type of the machine learning prediction.
     """
 
     global target_label
@@ -16,6 +17,7 @@ class PredictingMissedAppointmet (ProblemDefinition):
 
     target_label = 'status'
     target_entity = 'Appointment'
+    prediction_type = 'classification'
 
     def generate_cutoff_times(self, entity_set):
         """Generates cutoff times for the predection problem.
@@ -24,36 +26,34 @@ class PredictingMissedAppointmet (ProblemDefinition):
             entity_set: FHIR entityset.
 
         Returns:
-            DataFrame with entity id, cutoff times and target labels.
+            entity_set, target_entity, target_label and a dataframe of cutoff_times
 
         Raises:
-            ValueError: An error occurs if the target label contains a missing value.
-            ValueError: An error occurs if the target label does not exist.
-
+            ValueError: An error occurs if the cutoff variable does not exist.
         """
 
-        if self.check_target_label(
+        if (self.check_target_label(
                 ProblemDefinition,
                 entity_set,
                 target_entity,
-                target_label):
-            if self.check_target_label_values(
-                    ProblemDefinition, entity_set, target_entity, target_label):
-                raise ValueError('Please remove missing values in the target label')
+                target_label)) and not (self.check_target_label_values(
+                    ProblemDefinition,
+                    entity_set,
+                    target_entity,
+                    target_label)):
 
-            elif self.check_target_label(
+            if self.check_target_label(
                     ProblemDefinition,
                     entity_set,
                     target_entity,
                     'created'):  # check the existance of the cutoff time in the entity.
 
-                cutoff_times = entity_set[target_entity].df[[
-                    'identifier', 'created', target_label]].sort_values(by='created')
-                cutoff_times = cutoff_times.rename(
-                    columns={'created': 'cutoff_time', target_label: 'label'})
-                return cutoff_times
+                cutoff_times = entity_set[target_entity].df['created']
+                cutoff_times = cutoff_times.to_frame()
+                cutoff_times.index = entity_set[target_entity].df.index
+                cutoff_times = cutoff_times.rename(columns={'created': 'cutoff_time'})
+                return entity_set, target_entity, target_label, cutoff_times
             else:
-                raise ValueError('cutoff time variable does not exist')
-
-        else:
-            raise ValueError('Target label does not exist')
+                raise ValueError(
+                    'Cutoff time {} in {} does not exist'.format(
+                        'created', target_entity))
