19f08fe8ebdab99a9dcf16254c01b3f939a2e60f
sarahmish <sarahalnegheimish@gmail.com>
2018-10-16 17:09:11 -0400

    Added Diamond class

--- cardea/data_loader/__init__.py
+++ cardea/data_loader/__init__.py
@@ -1,6 +1,9 @@
 # import logging
 
-from cardea.data_loader.data_loader import DataLoader
+from cardea.data_loader.data_loader import DataLoader, Diamond
 from cardea.data_loader.entityset_loader import EntitySetLoader
 
-__all__ = ('DataLoader', 'EntitySetLoader')
+__all__ = (
+    'DataLoader',
+    'Diamond',
+    'EntitySetLoader')
