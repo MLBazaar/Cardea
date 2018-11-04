97b94506f438f140deeaceb7e7d5218d2f4c4761
Shahad Althobaiti <salthobaiti@kacst.edu.sa>
2018-11-04 12:47:25 -0500

    add imports for the new problem definitions

--- cardea/problem_definition/__init__.py
+++ cardea/problem_definition/__init__.py
@@ -1,6 +1,8 @@
 # import logging
 from cardea.problem_definition.definition import ProblemDefinition
 from cardea.problem_definition.length_of_stay import LengthOfStay
+from cardea.problem_definition.mortality_prediction import MortalityPrediction
+from cardea.problem_definition.predicting_diagnosis import DiagnosisPrediction
 from cardea.problem_definition.prolonged_length_of_stay import ProlongedLengthOfStay
 from cardea.problem_definition.readmission import Readmission
 from cardea.problem_definition.show_noshow_appointment import MissedAppointmentProblemDefinition
@@ -10,4 +12,6 @@ __all__ = (
     'MissedAppointmentProblemDefinition',
     'LengthOfStay',
     'ProlongedLengthOfStay',
-    'Readmission')
+    'Readmission',
+    'DiagnosisPrediction',
+    'MortalityPrediction')
