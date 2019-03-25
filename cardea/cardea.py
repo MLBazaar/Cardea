04ec2c65b89d6876d0ba0f889d9991688d4d947b
Sarah Alnegheimish <40212131+sarahmish@users.noreply.github.com>
2019-03-25 22:49:50 +0300

    Corrected docstrings Indentation

--- cardea/cardea.py
+++ cardea/cardea.py
@@ -16,15 +16,15 @@ from cardea.problem_definition import (
 class Cardea():
     """An interface class that ties the end-to-end system together.
 
-        Attributes:
-            es_loader: An entityset loader.
-            featurization: A featurization class.
-            modeler: A modeling class.
-            problems: A list of currently available prediction problems.
-            chosen_problem: The selected prediction problem or regression.
-            es: The loaded entityset.
-            target_entity: The target entity for featurization.
-        """
+    Attributes:
+        es_loader: An entityset loader.
+        featurization: A featurization class.
+        modeler: A modeling class.
+        problems: A list of currently available prediction problems.
+        chosen_problem: The selected prediction problem or regression.
+        es: The loaded entityset.
+        target_entity: The target entity for featurization.
+    """
 
     def __init__(self):
 
@@ -39,16 +39,16 @@ class Cardea():
     def load_data_entityset(self, folder_path=None):
         """Returns an entityset loaded with .csv files in folder_path.
 
-            Load the given dataset within the folder path into an entityset. The dataset
-            must be in a FHIR structure format. If no folder_path is not passed, the
-            function will automatically load kaggle's missed appointment dataset.
+        Load the given dataset within the folder path into an entityset. The dataset
+        must be in a FHIR structure format. If no folder_path is not passed, the
+        function will automatically load kaggle's missed appointment dataset.
 
-            Args:
-                folder_path: A directory of all .csv files that should be loaded.
+        Args:
+            folder_path: A directory of all .csv files that should be loaded.
 
-            Returns:
-                An entityset with loaded data.
-            """
+        Returns:
+            An entityset with loaded data.
+        """
 
         if folder_path:
             self.es = self.es_loader.load_data_entityset(folder_path)
@@ -73,9 +73,9 @@ class Cardea():
     def list_problems(self):
         """Returns a list of the currently available problems.
 
-            Returns:
-                A set of the available problems.
-            """
+        Returns:
+            A set of the available problems.
+        """
 
         problems = set([])
         for attribute_string in dir(cardea.problem_definition):
@@ -89,17 +89,17 @@ class Cardea():
     def select_problem(self, selection, parameter=None):
         """Select a prediction problem and extract information.
 
-            Update the select_problem attribute and generate the cutoff times,
-            the target entity and update the entityset.
+        Update the select_problem attribute and generate the cutoff times,
+        the target entity and update the entityset.
 
-            Args:
-                selection: Name of the chosen prediction problem.
-                data: Entityset representation of the data.
-                parameters: A variable to change the default parameters, if any.
+        Args:
+            selection: Name of the chosen prediction problem.
+            data: Entityset representation of the data.
+            parameters: A variable to change the default parameters, if any.
 
-            Returns:
-                The updated version of the entityset and cutoff time label.
-            """
+        Returns:
+            The updated version of the entityset and cutoff time label.
+        """
 
         # problem selection
         if selection == 'LengthOfStay':
@@ -139,21 +139,21 @@ class Cardea():
     def list_feature_primitives(self):
         """Returns built-in primitive in Featuretools.
 
-            Returns:
-                A pandas dataframe that lists and describes each built-in primitives.
-            """
+        Returns:
+            A pandas dataframe that lists and describes each built-in primitives.
+        """
         return ft.list_primitives()
 
     def generate_features(self, cutoff):
         """Returns a the calculated feature matrix.
 
-            Args:
-                es: A featuretools entityset that holds data.
-                cutoff: A pandas dataframe that indicates cutoff_time for each instance.
+        Args:
+            es: A featuretools entityset that holds data.
+            cutoff: A pandas dataframe that indicates cutoff_time for each instance.
 
-            Returns:
-                Encoded feature_matrix, encoded features.
-            """
+        Returns:
+            Encoded feature_matrix, encoded features.
+        """
 
         fm_encoded, _ = self.featurization.generate_feature_matrix(
             self.es, self.target_entity, cutoff)
@@ -164,18 +164,18 @@ class Cardea():
                       optimize=False, hyperparameters=None):
         """Executes and predict all the pipelines.
 
-            This method executes the given pipeline and returns a list for all the pipelines
-            with the result of each fold with its associated predicted values and actual values.
+        This method executes the given pipeline and returns a list for all the pipelines
+        with the result of each fold with its associated predicted values and actual values.
 
-            Args:
-                data_frame: A dataframe, which encapsulates all the records of that entity.
-                primitives_list: A list of the primitives within a pipeline.
-                optimize: A boolean value which indicates whether to optimize the model or not.
-                hyperparameters: A dictionary of hyperparameters for each primitives.
+        Args:
+            data_frame: A dataframe, which encapsulates all the records of that entity.
+            primitives_list: A list of the primitives within a pipeline.
+            optimize: A boolean value which indicates whether to optimize the model or not.
+            hyperparameters: A dictionary of hyperparameters for each primitives.
 
-            Returns:
-                A list for all the executed pipelines and its result.
-            """
+        Returns:
+            A list for all the executed pipelines and its result.
+        """
 
         return self.modeler.execute_pipeline(
             data_frame=feature_matrix,
@@ -189,21 +189,21 @@ class Cardea():
     def convert_to_json(dic):
         """Converts a given dictionary to json format.
 
-            Args:
-                dict: A dictionary of values to be coverted.
+        Args:
+            dict: A dictionary of values to be coverted.
 
-            Returns:
-                A string in json format.
-            """
+        Returns:
+            A string in json format.
+        """
         return json.dumps(dic)
 
     def convert_from_json(string):
         """Converts a given json string to dictionary format.
 
-            Args:
-                json: A dictionary of values to be coverted.
+        Args:
+            json: A dictionary of values to be coverted.
 
-            Returns:
-                A parsed dictionary.
-            """
+        Returns:
+            A parsed dictionary.
+        """
         return json.loads(string)
