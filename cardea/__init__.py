031377f84c09b974e8c15b9080db7a1f4401f422
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2019-04-17 14:38:06 +0300

    Correctly sorted imports

--- cardea/__init__.py
+++ cardea/__init__.py
@@ -5,7 +5,8 @@ __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
 __version__ = '0.0.1'
 
-from cardea.cardea import Cardea
 import logging
+
+from cardea.cardea import Cardea
 
 logging.getLogger('cardea').addHandler(logging.NullHandler())
