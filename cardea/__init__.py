e48f0faae90fe066b68a00be9942288bd1929253
Sarah Alnegheimish <sarahalnegheimish@gmail.com>
2019-04-17 14:24:52 +0300

    Fixed imports

--- cardea/__init__.py
+++ cardea/__init__.py
@@ -5,6 +5,7 @@ __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
 __version__ = '0.0.1'
 
+from cardea.cardea import Cardea
 import logging
 
 logging.getLogger('cardea').addHandler(logging.NullHandler())
