31b37ec852a7bb3f88a25f621370cb8a1ba1e5b5
sarahmish <sarahalnegheimish@gmail.com>
2020-09-08 23:30:21 +0300

    fix docs

--- setup.py
+++ setup.py
@@ -35,9 +35,11 @@ development_requires = [
     'watchdog>=0.8.3',
     
     # build docs
-    'm2r>=0.2.0',
-    'Sphinx>=1.7.1',
-    'sphinx_rtd_theme>=0.2.4',
+    'm2r>=0.2.0,<0.3',
+    'nbsphinx>=0.5.0,<0.7',
+    'Sphinx>=1.7.1,<3',
+    'sphinx_rtd_theme>=0.2.4,<0.5',
+    'autodocsumm>=0.1.10,<1',
     'recommonmark>=0.4.0',
     'ipython==6.5.0',
 
