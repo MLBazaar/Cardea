c93355c26ce8441ec0744415d552c012baa093b3
sarahmish <sarahalnegheimish@gmail.com>
2020-09-15 02:35:00 +0300

    Bump version: 0.1.0.dev2 → 0.1.0.dev3

--- cardea/__init__.py
+++ cardea/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.1.0.dev2'
+__version__ = '0.1.0.dev3'
 
 import logging
 
--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/Cardea',
-    version='0.1.0.dev2',
+    version='0.1.0.dev3',
     zip_safe=False,
 )
