51e0f4a23719fa99436f9a6c8c4b58b9601970df
sarahmish <sarahalnegheimish@gmail.com>
2020-09-15 02:23:06 +0300

    Bump version: 0.1.0.dev0 → 0.1.0.dev1

--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/Cardea',
-    version='0.1.0.dev0',
+    version='0.1.0.dev1',
     zip_safe=False,
 )
