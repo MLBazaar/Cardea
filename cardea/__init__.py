8f5730a268eb2e025ef1127d4acd59636ee99b14
sarahmish <sarahalnegheimish@gmail.com>
2020-09-14 14:44:56 +0300

    change development setup

--- cardea/__init__.py
+++ cardea/__init__.py
@@ -3,7 +3,7 @@
 
 __author__ = """MIT Data To AI Lab"""
 __email__ = 'dailabmit@gmail.com'
-__version__ = '0.0.1'
+__version__ = '0.1.0.dev0'
 
 import logging
 
--- setup.py
+++ setup.py
@@ -91,6 +91,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/D3-AI/Cardea',
-    version='0.0.2',
+    version='0.1.0.dev0',
     zip_safe=False,
 )
