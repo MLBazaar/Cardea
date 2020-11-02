e34b84d9d2d79e6cb1230cef6cc0bb9d9f2cdccf
ChengFR <459763742@qq.com>
2020-11-02 21:13:49 +0800

    update benchmark apis

--- cardea/benchmark.py
+++ cardea/benchmark.py
@@ -23,78 +23,82 @@ PROBLEM_TYPE = {
     'LOS': 'classification',
     'Mortality': 'classification',
     'Readmission': 'classification',
-    'los': 'classification',
-    'mortality': 'classification',
-    'readmission': 'classification'
+}
+
+TARGET_NAME = {
+    'LOS': 'label',
+    'Mortality': 'label',
+    'Readmission': 'label',
 }
 
 
 def _scoring_folds(folds, metrics):
     """Score each fold from the pipeline results.
     Args:
-        folds: dict, records from the prediction results of a fold of data.
-            The keys must include "Actual" and "predicted".
-        metrics: dict, keys are the name of the metrics and the values are metrics functions.
+        folds: list or dict, a list or a dictionary of pipeline results in each fold, the results
+        consists of a list of prediction values and a list of label values.
+        metrics: dict, a dictionary of metric functions indexed by metric names.
 
     Returns:
-        scores: dict, keys are the name of the metrics and the values are scores
-            from the corresponding metrics.
+        A dictionary of aggregated scores calculated from the fold results.
     """
     if isinstance(folds, dict):
         folds = [v for k, v in folds.items()]
 
-    fold_tuples = [(f['Actual'], f['predicted']) for f in folds]
-    scores = pd.DataFrame(
-        [{item: func(para[0], para[1]) for item, func in metrics.items()} for para in
-         fold_tuples]).mean().to_dict()
+    performance = pd.DataFrame([{item: func(f['Actual'], f['predicted'])
+                                 for item, func in metrics.items()} for f in folds])
+    scores = performance.mean().to_dict()
 
     return scores
 
 
 def _split_feature_label(dataset, target_name):
-    """Generate the feature array and the target array from the raw dataframe.
+    """Split the feature array and the target array.
 
     Args:
-        dataset: pd.DataFrame, raw dataset consists of the features and the labels.
+        dataset: pd.DataFrame, a dataframe consists of both feature values and target values.
         target_name: str, the name of the target column in the dataset.
 
     Returns:
-        X: 2-D np.array, the feature array.
-        y: 1-D np.array, the target array.
+        features: 2-D np.array, the feature array.
+        target: 1-D np.array, the target array.
     """
     dataset = dataset.copy(deep=True)
-    y = np.array(dataset.pop(target_name))
-    X = np.array(dataset)
-    return X, y
+    target = np.array(dataset.pop(target_name))
+    features = np.array(dataset)
+    return features, target
 
 
-def _evaluate_pipeline(name, primitives, dataset, problem, hyperparameters=None, optimize=False,
-                       metrics=CLASSIFICATION_METRICS, target_name='TARGET'):
-    """Evalute a pipeline's performance on a dataset with given metrics
+def _evaluate_pipeline(name, run_id, primitives, dataset, problem, hyperparameters=None,
+                       optimize=False, metrics=None):
+    """Evaluate a pipeline's performance on a target dataset with the given metrics.
 
     Args:
-        name: str, the name of the pipeline.
-        primitives: list, a list of primitives that make up the pipeline.
-        dataset: pd.DataFrame, dataset for evaluation.
-        problem: str, the name of the problem
-        hyperparameters: dict, the hyperparameter setting of the pipeline.
-            If hyperparameters is None, the pipeline will be initialized with default values.
-        optimize: boolean, whether to optimize the hyperparameters used in the pipeline.
-        metrics: dict, keys are the name of the metrics and the values are metrics functions.
-        target_name: str, the name of the target column in the dataset.
+        name: str, the name to specify the pipeline.
+        run_id: int, the index to specify the execution to the pipeline.
+        primitives: list, A list of the primitives within a pipeline.
+        dataset: pd.DataFrame, a dataframe consists of both feature values and target values.
+        problem: str, the name to specify the problem.
+        hyperparameters: dict, A dictionary of hyper-parameters for each primitive.
+        optimize: boolean, whether to optimize the hyper-parameters of the pipeline.
+        metrics: dict, a dictionary in which metric functions are indexed by names.
 
     Returns:
         scores: dict, keys include "Pipeline", "Problem", "Tuned", "Elapsed Time(s)", "Status",
             and metric names.
     """
-    X, y = _split_feature_label(dataset, target_name)
+    features, target = _split_feature_label(dataset, TARGET_NAME[problem])
+    if metrics is None:
+        metrics = CLASSIFICATION_METRICS
     modeler = Modeler()
 
     LOGGER.info("Starting pipeline {} for {} problem..".format(name, problem))
 
+    start = datetime.utcnow()
     try:
-        start = datetime.utcnow()
-        pipelines_res = modeler.execute_pipeline(X, y, [primitives], PROBLEM_TYPE[problem],
+        pipelines_res = modeler.execute_pipeline(np.array(features).astype(np.float),
+                                                 np.array(target).astype(np.float),
+                                                 [primitives], PROBLEM_TYPE[problem],
                                                  optimize=optimize,
                                                  hyperparameters=hyperparameters,
                                                  minimize_cost=False, scoring='f1',
@@ -115,6 +119,7 @@ def _evaluate_pipeline(name, primitives, dataset, problem, hyperparameters=None,
         scores['Status'] = 'Fail'
 
     scores['Pipeline'] = name
+    scores['Run #'] = run_id
     scores['Problem'] = problem
     scores['Tuned'] = optimize or hyperparameters is not None
 
@@ -122,24 +127,25 @@ def _evaluate_pipeline(name, primitives, dataset, problem, hyperparameters=None,
 
 
 def _evaluate_pipelines(pipelines, dataset, problem, hyperparameters=None, optimize=False,
-                        metrics=CLASSIFICATION_METRICS, target_name='TARGET', runs=1):
-    """Evalutate pipelines on a dataset with given metrics.
+                        metrics=None, runs=1):
+    """Evaluate pipelines on a dataset with given metrics.
 
     Args:
-        pipelines: list, a lisf of pipelines.
-        dataset: pd.DataFrame, dataset for evaluation.
-        problem: str, the name of the problem
-        hyperparameters: dict, the hyperparameter setting of the pipeline.
-            If hyperparameters is None, the pipeline will be initialized with default values.
-        optimize: boolean, whether to optimize the hyperparameters used in the pipeline.
-        metrics: dict, keys are the name of the metrics and the values are metrics functions.
-        target_name: str, the name of the target column in the dataset.
-        runs: int, the number of runs of the each pipeline.
+        pipelines: list or dict, a list or a dictionary of the pipelines.
+        dataset: pd.DataFrame, a dataframe consists of both feature values and target values.
+        problem: str, the name to specify the problem.
+        hyperparameters: list or dict, a list or a dictionary of hyper-parameters for each
+        pipeline.
+        optimize: boolean, whether to optimize the hyper-parameters of the pipelines.
+        metrics: dict, a dict in which metric functions are indexed by the metric names.
+        runs: int, the number of executions to each pipeline.
 
     Returns:
-        score_list, list. A list of pipeline scores (dict).
+        A list of pipeline performance dicts.
     """
-    score_list = []
+    if metrics is None:
+        metrics = CLASSIFICATION_METRICS
+
     if isinstance(pipelines, list):
         pipelines = {"pipeline_{}".format(i): pipeline for i, pipeline in enumerate(pipelines)}
 
@@ -148,74 +154,86 @@ def _evaluate_pipelines(pipelines, dataset, problem, hyperparameters=None, optim
     elif isinstance(hyperparameters, list):
         hyperparameters = {"pipeline_{}".format(i): hyper for i, hyper in
                            enumerate(hyperparameters)}
-
+    performance = []
     for name, pipeline in pipelines.items():
-        for _ in range(runs):
-            score_list.append(
-                _evaluate_pipeline(name, pipeline, dataset, problem, hyperparameters[name],
-                                   optimize, metrics,
-                                   target_name))
+        for run_id in range(runs):
+            performance.append(_evaluate_pipeline(name, run_id, pipeline, dataset, problem,
+                                                  hyperparameters[name], optimize, metrics))
 
-    return score_list
+    return performance
 
 
-def aggregate_results_by_pipeline(df, metric):
+def aggregate_results_by_pipeline(performance, metric, record_time=True, output_path=None):
     """Aggregate the results of each pipeline.
 
     Args:
-        df: pd.DataFrame, the results of all runs.
-        metric: str, the name of the metric for aggregation.
+        performance: pd.DataFrame, the performance of each pipeline execution.
+        metric: str, the name of the target metric for summary.
+        record_time: boolean, whether to the record the elapsed time in the summary.
+        output_path: str, the path to store the results.
 
     Returns:
-        aggr_df, pd.DataFrame, each row records the aggregated scores of all pipelines
-            on a dataset (problem).
+        A pd.DataFrame object that stores the aggregated performance of each pipeline on
+        each problem.
     """
-    if 'Status' in df.columns:
-        df = df[df['Status'] == 'OK']
+    if 'Status' in performance.columns:
+        performance = performance[performance['Status'] == 'OK']
 
-    problems = df['Problem'].unique()
-    pipelines = df['Pipeline'].unique()
+    problems = performance['Problem'].unique()
+    pipelines = performance['Pipeline'].unique()
 
     aggr_results = []
     for problem in problems:
         problem_dict = {}
         for pipeline in pipelines:
-            runs = _select_runs(df, problem, pipeline)
+            runs = _select_runs(performance, problem, pipeline)
             problem_dict["{}_Average {}".format(pipeline, metric)] = runs[metric].mean()
             problem_dict["{}_Best {}".format(pipeline, metric)] = runs[metric].max()
-            problem_dict["{}_Average Elapsed Time(s)".format(pipeline)] = runs[
-                "Elapsed Time(s)"].mean()
+            if record_time:
+                problem_dict["{}_Average Elapsed Time(s)".format(pipeline)] = \
+                    runs["Elapsed Time(s)"].mean()
         aggr_results.append(problem_dict)
     aggr_df = pd.DataFrame(aggr_results, index=problems)
     aggr_df.columns = aggr_df.columns.str.split('_', expand=True)
 
+    if output_path:
+        aggr_df.to_csv(output_path)
+
     return aggr_df
 
 
-def aggregate_results_by_problem(df, metric):
+def aggregate_results_by_problem(performance, metric, record_time=True, output_path=None):
     """Aggregate the results on each problem (dataset).
 
     Args:
-        df: pd.DataFrame, the results of all runs.
-        metric: str, the name of the metric for aggregation.
+        performance: pd.DataFrame, the performance of each pipeline execution.
+        metric: str, the name of the target metric for summary.
+        record_time: boolean, whether to the record the elapsed time in the summary.
+        output_path: str, the path to store the results.
 
     Returns:
-        aggr_df, pd.DataFrame, each row records the aggregated scores on a dataset (problem).
+        A pd.DataFrame object that stores the aggregated performance on each problem.
     """
-    if 'Status' in df.columns:
-        df = df[df['Status'] == 'OK']
+    if 'Status' in performance.columns:
+        performance = performance[performance['Status'] == 'OK']
 
-    problems = df['Problem'].unique()
+    problems = performance['Problem'].unique()
     aggr_results = []
     for problem in problems:
-        runs = _select_runs(df, problem)
+        runs = _select_runs(performance, problem)
         problem_dict = {
             "Average {}".format(metric): runs[metric].mean(),
             "Best {}".format(metric): runs[metric].max(),
             "Best Pipeline": runs.loc[runs[metric].idxmax(), 'Pipeline']
         }
+        if record_time:
+            problem_dict["Average Elapsed Time(s)"] = runs["Elapsed Time(s)"].mean()
         aggr_results.append(problem_dict)
     aggr_df = pd.DataFrame(aggr_results, index=problems)
+
+    if output_path:
+        aggr_df.to_csv(output_path)
+
     return aggr_df
 
 
@@ -227,40 +245,49 @@ def _select_runs(df, problem=None, pipeline=None):
     return df
 
 
-def benchmark(pipelines, datasets, problems, hyperparameters=None, optimize=False,
-              metrics=CLASSIFICATION_METRICS, target_name='TARGET', runs=1, from_fm=True,
-              output_path=None):
-    """Evalutate pipelines on a set of datasets (problems) with given metrics.
+def benchmark(pipelines, datasets, problems=None, hyperparameters=None, optimize=False,
+              metrics=None, runs=1, stage='data_loader', output_path=None):
+    """Evaluate pipelines on a set of datasets (problems) with given metrics.
 
     Args:
-        pipelines: list, a lisf of pipelines.
-        datasets: dict, keys are the name of the problem (str), values are the dataset
-            for evaluation (pd.DataFrame).
+        pipelines: list or dict, a list or a dictionary of pipelines.
+        datasets: str or dict,
+            * str, path to the data folder when the stage is "data_loader" or 0;
+            * str, path to the entity pickle file when the stage is "problem_definition" or 1;
+            * dict, a dictionary in which feature matrices (pd.DataFrame) are indexed by a problem
+            name when the stage is "featurization" or 2.
         problems: list, a list of problem names (str).
-        hyperparameters: dict, the hyperparameter setting of the pipeline.
-            If hyperparameters is None, the pipeline will be initialized with default values.
-        optimize: boolean, whether to optimize the hyperparameters used in the pipeline.
-        metrics: dict, keys are the name of the metrics and the values are metrics functions.
-        target_name: str, the name of the target column in the dataset.
-        runs: int, the number of runs of the each pipeline.
-        from_fm: boolean, whether the datasets are feature matrix data.
-        output_path: str, path of a csv file to store the results.
+        hyperparameters: list or dict, a list or a dictionary of hyper-parameters for each
+        pipeline.
+        optimize: boolean, whether to optimize the hyper-parameters of the pipeline.
+        metrics: dict, a dictionary in which metric functions are indexed by the metric names.
+        runs: int, the number of executions to each pipeline on each problem.
+        stage: enumerate (int or str), the stage in which the benchmarking are applied, should be
+            either "data_loader", "problem_definition", "featurization", or an index (int).
+            0 is for "data_loader", 1 is for "problem_definition", 2 is for "featurization".
+        output_path: str, the path to store the results.
 
     Returns:
-        result_df: pd.DataFrame, the results of all runs.
+        A pd.DataFrame object that stores the performance of each pipeline execution.
     """
-    if from_fm:
-        if isinstance(datasets, list):
-            datasets = {p: d for p, d in zip(problems, datasets)}
-    else:
-        raise NotImplementedError
+    if metrics is None:
+        metrics = CLASSIFICATION_METRICS
 
-    score_list = []
-    for problem, dataset in datasets.items():
-        score_list.extend(
-            _evaluate_pipelines(pipelines, dataset, problem, hyperparameters, optimize, metrics,
-                                target_name, runs))
-    result_df = pd.DataFrame.from_records(score_list)
+    if stage == "data_loader" or stage == 0:
+        raise NotImplementedError
+    elif stage == "problem_definition" or stage == 1:
+        raise NotImplementedError
+    elif stage == "featurization" or stage == 2:
+        feature_matrices = datasets
+    else:
+        raise ValueError("stage should be either \"data_loader\", \"problem_definition\" "
+                         "or \"featurization\".")
+
+    performance = []
+    for problem, dataset in feature_matrices.items():
+        performance.extend(_evaluate_pipelines(pipelines, dataset, problem, hyperparameters,
+                                               optimize, metrics, runs))
+    result_df = pd.DataFrame.from_records(performance)
 
     if output_path:
         result_df.to_csv(output_path)
