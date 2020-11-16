0e751716aa098477196fa507fede4efcb7e20633
ChengFR <459763742@qq.com>
2020-11-16 23:56:59 +0800

    add apis: benchmark_from_tasks(...), evaluate_task(...),  that allow users to benchmark cardea from task/tasks

---
+++ cardea/benchmark/benchmark.py
@@ -0,0 +1,312 @@
+import json
+import os
+import logging
+import shutil
+from datetime import datetime
+
+import numpy as np
+import pandas as pd
+import sklearn
+
+from mlblocks import MLPipeline
+from cardea.modeling.modeler import Modeler
+
+LOGGER = logging.getLogger(__name__)
+
+ROOT_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
+PIPELINE_DIR = os.path.join(ROOT_DIR, 'cardea', 'pipelines')
+VERIFIED_DIR = os.path.join(ROOT_DIR, 'benchmark', 'verified')
+
+CLASSIFICATION_METRICS = {
+    'F1 Macro': lambda *args, **kwargs: sklearn.metrics.f1_score(*args, **kwargs, average="macro"),
+    'Recall': lambda *args, **kwargs: sklearn.metrics.recall_score(*args, **kwargs,
+                                                                   average="macro"),
+    'Precision': lambda *args, **kwargs: sklearn.metrics.precision_score(*args, **kwargs,
+                                                                         average="macro"),
+    'Accuracy': sklearn.metrics.accuracy_score,
+    'Confusion Matrix': sklearn.metrics.confusion_matrix
+}
+
+PROBLEM_TYPE = {
+    'LOS': 'classification',
+    'Mortality': 'classification',
+    'Readmission': 'classification',
+}
+
+TARGET_NAME = {
+    'LOS': 'label',
+    'Mortality': 'label',
+    'Readmission': 'label',
+}
+
+
+def _scoring_folds(folds, metrics):
+    """Score each fold from the pipeline results.
+    Args:
+        folds: list or dict, a list or a dictionary of pipeline results in each fold, the results
+        consists of a list of prediction values and a list of label values.
+        metrics: dict, a dictionary of metric functions indexed by metric names.
+
+    Returns:
+        A dictionary of aggregated scores calculated from the fold results.
+    """
+    if isinstance(folds, dict):
+        folds = [v for k, v in folds.items()]
+
+    performance = pd.DataFrame([{item: func(f['Actual'], f['predicted'])
+                                 for item, func in metrics.items()} for f in folds])
+    scores = performance.mean().to_dict()
+
+    return scores
+
+
+def aggregate_results_by_pipeline(performance, metric, record_time=True, output_path=None):
+    """Aggregate the results of each pipeline.
+
+    Args:
+        performance: pd.DataFrame, the performance of each pipeline execution.
+        metric: str, the name of the target metric for summary.
+        record_time: boolean, whether to the record the elapsed time in the summary.
+        output_path: str, the path to store the results.
+
+    Returns:
+        A pd.DataFrame object that stores the aggregated performance of each pipeline on
+        each problem.
+    """
+    if 'Status' in performance.columns:
+        performance = performance[performance['Status'] == 'OK']
+
+    problems = performance['Problem Name'].unique()
+    pipelines = performance['Pipeline Name'].unique()
+
+    aggr_results = []
+    for problem in problems:
+        problem_dict = {}
+        for pipeline in pipelines:
+            runs = _select_runs(performance, problem, pipeline)
+            problem_dict["{}_Average {}".format(pipeline, metric)] = runs[metric].mean()
+            problem_dict["{}_Best {}".format(pipeline, metric)] = runs[metric].max()
+            if record_time:
+                problem_dict["{}_Average Elapsed Time(s)".format(pipeline)] = \
+                    runs["Elapsed Time(s)"].mean()
+        aggr_results.append(problem_dict)
+    aggr_df = pd.DataFrame(aggr_results, index=problems)
+    aggr_df.columns = aggr_df.columns.str.split('_', expand=True)
+
+    if output_path:
+        aggr_df.to_csv(output_path)
+
+    return aggr_df
+
+
+def aggregate_results_by_problem(performance, metric, record_time=True, output_path=None):
+    """Aggregate the results on each problem (dataset).
+
+    Args:
+        performance: pd.DataFrame, the performance of each pipeline execution.
+        metric: str, the name of the target metric for summary.
+        record_time: boolean, whether to the record the elapsed time in the summary.
+        output_path: str, the path to store the results.
+
+    Returns:
+        A pd.DataFrame object that stores the aggregated performance on each problem.
+    """
+    if 'Status' in performance.columns:
+        performance = performance[performance['Status'] == 'OK']
+
+    problems = performance['Problem'].unique()
+    aggr_results = []
+    for problem in problems:
+        runs = _select_runs(performance, problem)
+        problem_dict = {
+            "Average {}".format(metric): runs[metric].mean(),
+            "Best {}".format(metric): runs[metric].max(),
+            "Best Pipeline": runs.loc[runs[metric].idxmax(), 'Pipeline']
+        }
+        if record_time:
+            problem_dict["Average Elapsed Time(s)"] = runs["Elapsed Time(s)"].mean()
+        aggr_results.append(problem_dict)
+    aggr_df = pd.DataFrame(aggr_results, index=problems)
+
+    if output_path:
+        aggr_df.to_csv(output_path)
+
+    return aggr_df
+
+
+def _select_runs(df, problem=None, pipeline=None):
+    if isinstance(problem, str):
+        df = df[df['Problem Name'] == problem]
+    if isinstance(pipeline, str):
+        df = df[df['Pipeline Name'] == pipeline]
+    return df
+
+
+def benchmark_from_tasks(tasks, metrics=None, results_output_path=None, tasks_output_dir=None,
+                         save_model=True, save_intermedia_data=True, save_hyperparameters=True):
+    """
+    Args:
+        tasks: list, a list of task instances storing meta information of each task.
+        metrics: dict, a dictionary of metric functions indexed by metric names.
+        results_output_path: str, the csv file path to store the benchmarking results.
+        tasks_output_dir: dirt, the dir path to store the intermedia data, model and
+            hyperparametes of each task.
+        save_intermedia_data: boolean, whether to store the intermedia data including an entity set
+            and a feature matrix if the beginning stage is "data_loader" or "problem_definition".
+        save_model: boolean, whether to store the trained model.
+        save_hyperparameters: boolean, whether to store the hyperparameters if task.tuned is true.
+
+    Returns:
+        A pd.DataFrame object that stores the benchmarking results in detail.
+    """
+    if tasks_output_dir is not None:
+        if os.path.exists(tasks_output_dir):
+            shutil.rmtree(tasks_output_dir)
+        os.mkdir(tasks_output_dir)
+
+    performance = []
+    for task in tasks:
+        if tasks_output_dir is not None:
+            output_path = os.path.join(tasks_output_dir, task.task_id)
+        else:
+            output_path = None
+        performance.extend(evaluate_task(task=task, metrics=metrics, output_path=output_path,
+                                         save_model=save_model,
+                                         save_intermedia_data=save_intermedia_data,
+                                         save_hyperparameters=save_hyperparameters))
+    result_df = pd.DataFrame.from_records(performance)
+
+    if results_output_path is not None:
+        result_df.to_csv(results_output_path)
+
+    return result_df
+
+
+def evaluate_task(task, metrics=None, output_path=None, save_intermedia_data=True,
+                  save_model=True, save_hyperparameters=True):
+    """
+    Args:
+        task: Task, a task instance storing meta information of the task.
+        metrics: dict, a dictionary of metric functions indexed by metric names.
+        output_path: str, a dir path to store the intermedia data, model and hyperparametes.
+        save_intermedia_data: boolean, whether to store the intermedia data including an entity set
+            and a feature matrix if the beginning stage is "data_loader" or "problem_definition".
+        save_model: boolean, whether to store the trained model.
+        save_hyperparameters: boolean, whether to store the hyperparameters if task.tuned is true.
+
+    Returns:
+        A list of benchmarking results of each run.
+    """
+    if metrics is None:
+        metrics = CLASSIFICATION_METRICS
+
+    # Load pipeline.
+    with open(task.path_to_pipeline) as f:
+        pipeline = MLPipeline.from_dict(json.load(f))
+
+    # Set hyperparameters.
+    if task.init_hyperparameters is not None:
+        pipeline.set_hyperparameters(task.init_hyperparameters)
+
+    # Load Dataset.
+    if task.beginning_stage == "data_loader":
+        raise NotImplementedError
+
+    elif task.beginning_stage == "problem_definition":
+        raise NotImplementedError
+
+    elif task.beginning_stage == "featurization":
+        feature_matrix = pd.read_csv(task.path_to_dataset)
+
+    else:
+        raise ValueError("Beginning stage should be either \"data_loader\", "
+                         "\"problem_definition\" or \"featurization\".")
+
+    # Run the pipeline for #task.run_num times and record each run.
+    results = []
+    for i in range(task.run_num):
+        scores, _, hyperparameters = _evaluate_pipeline(i, pipeline, feature_matrix,
+                                                        task.pipeline_name,
+                                                        task.problem_name,
+                                                        task.dataset_name,
+                                                        task.beginning_stage,
+                                                        task.tuned, metrics)
+        results.append(scores)
+
+    # Store the output results.
+    if output_path is not None:
+        ...
+
+    return results
+
+
+def _evaluate_pipeline(run_id, pipeline, feature_matrix, pipeline_name, problem_name,
+                       dataset_name, beginning_stage, optimize=False, metrics=None):
+    """Evaluate a pipeline's performance on a target dataset with the given metrics.
+
+    Args:
+        run_id: int, the index to specify the execution to the pipeline.
+        pipeline: MLPipeline, a pipeline instance.
+        feature_matrix: pd.DataFrame, a dataframe consists of both feature values
+            and target values.
+        pipeline_name: str, the name of the pipeline.
+        problem_name: str, the name of the problem.
+        dataset_name: str, the name of the dataset.
+        beginning_stage: enumerate, the stage in which the benchmarking are applied, should be
+            either "data_loader", "problem_definition", "featurization".
+        optimize: boolean, whether to optimize the hyper-parameters of the pipeline.
+        metrics: dict, a dictionary in which metric functions are indexed by names.
+
+    Returns:
+        A tuple includes performance stored in a dictionary, a pipeline model,
+            and hyperparameters.
+    """
+    # features, target = _split_feature_label(feature_matrix, TARGET_NAME[problem_name])
+    features = feature_matrix.copy()
+    features = features.sample(3000).reset_index(drop=True)
+    target = features.pop(TARGET_NAME[problem_name])
+
+    if problem_name == 'LOS' and dataset_name == 'mimic-iii-ft':
+        target = np.digitize(target, [0, 7])
+
+    if metrics is None:
+        metrics = CLASSIFICATION_METRICS
+
+    modeler = Modeler()
+
+    LOGGER.info("Starting pipeline {} for {} problem..".format(pipeline_name, problem_name))
+
+    start = datetime.utcnow()
+    try:
+        pipelines_res = modeler.execute_pipeline_from_pipeline(features, target, [pipeline],
+                                                               PROBLEM_TYPE[problem_name],
+                                                               optimize=optimize,
+                                                               minimize_cost=False, scoring='f1',
+                                                               max_evals=10)
+        elapsed = datetime.utcnow() - start
+        hyperparameters = pipelines_res['pipeline0']['hyperparameter']
+        # model = pipelines_res['pipeline0']['model']
+        scores = _scoring_folds(pipelines_res['pipeline0']['folds'], metrics)
+        scores['Elapsed Time(s)'] = elapsed.total_seconds()
+        scores['Status'] = 'OK'
+
+    except Exception as ex:
+        LOGGER.exception(
+            "Exception scoring pipeline {} in problem {}, exception {}".format(pipeline_name,
+                                                                               problem_name, ex))
+        elapsed = datetime.utcnow() - start
+        hyperparameters = None
+        scores = {
+            name: 0 for name in metrics.keys()
+        }
+        scores['Elapsed Time(s)'] = elapsed.total_seconds()
+        scores['Status'] = 'Fail'
+
+    scores['Pipeline Name'] = pipeline_name
+    scores['Run #'] = run_id
+    scores['Problem Name'] = problem_name
+    scores['Dataset Name'] = dataset_name
+    scores['Beginning Stage'] = beginning_stage
+    scores['Tuned'] = optimize
+
+    return scores, None, hyperparameters
---
+++ cardea/benchmark/task.py
@@ -0,0 +1,193 @@
+import json
+import os
+import pickle
+import shutil
+import logging
+
+from collections import OrderedDict
+
+PIPELINES = ['Logistic Regression', 'K-Nearest Neightbors', 'Random Forest',
+             'Gaussian Naive Bayes', 'Multinomial Naive Bayes', 'XGB',
+             'Stochastic Gradient Descent', 'Gradient Boosting']
+
+PROBLEMS = ['LOS', 'Mortality', 'Readmission']
+
+ROOT_DIR = os.path.abspath(os.path.join(__file__, '../../../'))
+PIPELINE_DIR = os.path.join(ROOT_DIR, 'cardea', 'pipelines')
+VERIFIED_DIR = os.path.join(ROOT_DIR, 'benchmark', 'verified')
+
+
+class Task:
+    def __init__(self, task_id=None, pipeline_name=None, path_to_pipeline=None,
+                 beginning_stage=None, dataset_name=None, problem_name=None, path_to_dataset=None,
+                 init_hyperparameters=None, tuned=None, run_num=None, ):
+        self._task_id = task_id
+        self._pipeline_name = pipeline_name
+        self._beginning_stage = beginning_stage
+        self._dataset_name = dataset_name
+        self._problem_name = problem_name
+        self._path_to_dataset = path_to_dataset
+        self._path_to_pipeline = path_to_pipeline
+        self._init_hyperparameters = init_hyperparameters
+        self._tuned = tuned
+        self._run_num = run_num
+
+    def __str__(self):
+        description_str = ""
+        ordered_items = list(sorted(self.__dict__.items(), key=lambda x: x[0]))
+        for k, v in ordered_items:
+            description_str += "{:<20} {}\n".format(k, v)
+        return description_str
+
+    def __repr__(self):
+        description_str = ""
+        ordered_items = list(sorted(self.__dict__.items(), key=lambda x: x[0]))
+        for k, v in ordered_items:
+            description_str += "{:<20} {}\n".format(k, v)
+        return description_str
+
+    def save_as(self, file_path, file_type='pkl'):
+        if file_type == 'pkl':
+            with open(file_path, 'wb') as f:
+                pickle.dump(self, f)
+        elif file_type == 'json':
+            with open(file_path, 'w') as f:
+                json.dump(self.__dict__, f)
+        else:
+            raise NotImplementedError
+
+    @staticmethod
+    def load(file_path):
+        with open(file_path, 'f') as f:
+            attr_dict = json.load(f)
+        return Task(**attr_dict)
+
+    @property
+    def task_id(self):
+        return self._task_id
+
+    @property
+    def pipeline_name(self):
+        return self._pipeline_name
+
+    @property
+    def dataset_name(self):
+        return self._dataset_name
+
+    @property
+    def problem_name(self):
+        return self._problem_name
+
+    @property
+    def beginning_stage(self):
+        return self._beginning_stage
+
+    @property
+    def path_to_pipeline(self):
+        return self._path_to_pipeline
+
+    @path_to_pipeline.setter
+    def path_to_pipeline(self, path_to_pipeline):
+        self._path_to_pipeline = path_to_pipeline
+
+    @property
+    def path_to_dataset(self):
+        return self._path_to_dataset
+
+    @path_to_dataset.setter
+    def path_to_dataset(self, path_to_dataset):
+        self._path_to_dataset = path_to_dataset
+
+    @property
+    def init_hyperparameters(self):
+        return self._init_hyperparameters
+
+    @init_hyperparameters.setter
+    def init_hyperparameters(self, init_hyperparameters):
+        self._init_hyperparameters = init_hyperparameters
+
+    def load_hyperparameters(self, path_to_hyperparameters):
+        with open(path_to_hyperparameters, 'r') as f:
+            self._init_hyperparameters = json.load(f)
+
+    @property
+    def tuned(self):
+        return self._tuned
+
+    @property
+    def run_num(self):
+        return self._run_num
+
+
+def create_tasks(pipeline_names=None, problem_names=None, dataset_name='MIMIC-III',
+                 beginning_stage='data_loader', optimize=False, run_num=1, output_dir=None):
+    """
+    Args:
+        pipeline_names: list, a list of strings that verify the pipelines for testing.
+        problem_names: list, a list of strings that verify the problems to be tested on.
+        dataset_name: str, name of the dataset.
+        beginning_stage: enumerate, the stage in which the benchmarking are applied, should be
+            either "data_loader", "problem_definition", "featurization".
+        optimize: boolean, whether to optimize the hyper-parameters of the pipeline.
+        run_num: int, the number of executions to each pipeline on each problem.
+        output_dir: str, the path to store the task configurations.
+
+    Returns:
+        a list of Task objects that store the configurations of the tasks.
+    """
+    if pipeline_names is None:
+        pipeline_names = PIPELINES
+    pipeline_names = sorted(pipeline_names)
+
+    if problem_names is None:
+        problem_names = PROBLEMS
+    problem_names = sorted(problem_names)
+
+    task_num = 0
+    tasks = []
+    for problem in problem_names:
+        for pipeline in pipeline_names:
+            task_id = "{}_{}_{}".format(task_num, problem, pipeline)
+            task = Task(task_id=task_id, pipeline_name=pipeline, dataset_name=dataset_name,
+                        problem_name=problem, beginning_stage=beginning_stage,
+                        run_num=run_num, tuned=optimize)
+            task.path_to_pipeline = os.path.join(PIPELINE_DIR,
+                                                 '{}.json'.format(task.pipeline_name))
+            if beginning_stage == 'data_loader':
+                task.path_to_dataset = os.path.join(VERIFIED_DIR, 'Raw', task.dataset_name)
+            elif beginning_stage == 'problem_definition':
+                task.path_to_dataset = os.path.join(VERIFIED_DIR, 'EntitySet',
+                                                    '.pkl'.format(task.dataset_name))
+            elif beginning_stage == 'featurization':
+                task.path_to_dataset = os.path.join(VERIFIED_DIR, 'Problems', task.problem_name,
+                                                    'FeatureMatrix',
+                                                    '{}.csv'.format(task.dataset_name))
+            tasks.append(task)
+            task_num += 1
+
+    if output_dir is not None:
+        if os.path.exists(output_dir):
+            shutil.rmtree(output_dir)
+        os.mkdir(output_dir)
+        for task in tasks:
+            task_path = os.path.join(output_dir, task.task_id)
+            os.mkdir(task_path)
+            task.save_as(os.path.join(task_path, 'meta.json', 'json'))
+
+    return tasks
+
+
+def load_tasks(root_folder=None):
+    """
+    Args:
+        root_folder: str, the path where the task configurations are stored.
+
+    Returns:
+         a list of Task objects that store the configurations of the tasks.
+    """
+    task_folders = [f for f in os.listdir(root_folder) if os.path.isdir(f)]
+    tasks = []
+    for folder in task_folders:
+        with open(os.path.join(folder, 'config.pkl'), 'rb') as f:
+            tasks.append(pickle.load(f))
+    return tasks
