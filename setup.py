5d556c4231f4e625434f14e9f35153ef337f1e26
sarahmish <sarahalnegheimish@gmail.com>
2020-12-11 17:34:45 -0500

    Bump version: 0.1.1 → 0.1.2.dev0

--- setup.py
+++ setup.py
@@ -100,6 +100,6 @@ setup(
     test_suite='tests',
     tests_require=tests_require,
     url='https://github.com/DAI-Lab/Cardea',
-    version='0.1.1',
+    version='0.1.2.dev0',
     zip_safe=False
 )
