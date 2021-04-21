b7eeeb8afaf00eb33f0e9fb0d7e2f6fb58660378
sarahmish <sarahalnegheimish@gmail.com>
2021-04-21 12:17:16 -0400

    update python version description

--- setup.py
+++ setup.py
@@ -68,9 +68,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
     description="Cardea",
     entry_points={
